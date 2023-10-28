ad7a3224cfd7ddebf53af01fcdc76de2ead40c4b
William Almy <william.almy@skydio.com>
2023-10-27 23:53:33 -0700

    Initialize node_streaming

--- src/node_streaming/__init__.py
+++ src/node_streaming/__init__.py
@@ -1,17 +1,26 @@
+import os
+
+from web3 import Web3
+
+NODE_URL = os.getenv("NODE_URL", None)
+if not NODE_URL:
+    raise ValueError("Must set NODE_URL in environment variables")
+
+
 class NodeStreaming:
     """
     TODO @will Adjust signatures as needed.
     This modules takes in a list of contract addresses and their corresponding log events to watch.
-
-
     """
 
     def __init__(self):
-        pass
+        self.w3 = Web3(Web3.HTTPProvider(NODE_URL))
+        assert self.w3.is_connected()
 
-    def start_monitor(self, start_block_number: int, watching_contracts: dict[str, list[str]]):
+    def start_monitor(
+        self, start_block_number: int, watching_contracts: dict[str, list[str]]
+    ):
         """
-
         :param start_block_number: the starting block to streaming for
         :param watching_contracts:
             a dict of contracts to a list of log events
@@ -31,3 +40,7 @@ class NodeStreaming:
 
     def stop(self):
         pass
+
+
+if __name__ == "__main__":
+    node_stream = NodeStreaming()
