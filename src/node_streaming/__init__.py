2ea3db2e8c2716fb2ddcc37bc2a399966a38c888
zeidsolh <74027562+zeidsolh@users.noreply.github.com>
2023-11-04 12:58:18 -0700

    Add files via upload

--- src/node_streaming/__init__.py
+++ src/node_streaming/__init__.py
@@ -1,46 +1,110 @@
 import os
-
+import time
+from dataclasses import dataclass
+from typing import List, Dict
+from dotenv import load_dotenv
 from web3 import Web3
 
-NODE_URL = os.getenv("NODE_URL", None)
+load_dotenv()
+
+NODE_URL = os.getenv("NODE_URL")
 if not NODE_URL:
     raise ValueError("Must set NODE_URL in environment variables")
+else:
+    print(f"Using NODE_URL: {NODE_URL}")
 
+@dataclass
+class ContractEvent:
+    address: str
+    abi: object
+    event: str
 
 class NodeStreaming:
-    """
-    TODO @will Adjust signatures as needed.
-    This modules takes in a list of contract addresses and their corresponding log events to watch.
-    """
-
     def __init__(self):
+        print("Initializing NodeStreaming...")
         self.w3 = Web3(Web3.HTTPProvider(NODE_URL))
-        assert self.w3.is_connected()
+        if not self.w3.is_connected():
+            raise ConnectionError("Unable to connect to the node at URL: " + NODE_URL)
+        else:
+            print("Connected to Ethereum node.")
 
-    def start_monitor(
-        self, start_block_number: int, watching_contracts: dict[str, list[str]]
-    ):
-        """
-        :param start_block_number: the starting block to streaming for
-        :param watching_contracts:
-            a dict of contracts to a list of log events
-            e.g
-            {"0x012...12" : ["Swap", "Sync"],
-            ...
-            }
-        :return:
-        """
-        pass
+        self.events_queue = []
+        self.events_filters = []
+    
+    def get_latest_block_number(self):
+        return self.w3.eth.block_number
+    
+    def fetch_contract_abi(self, contract_address: str):
+        if contract_address == "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0":
+            return [{
+                "anonymous": False,
+                "inputs": [
+                    {"indexed": True, "internalType": "address", "name": "sender", "type": "address"},
+                    {"indexed": False, "internalType": "uint256", "name": "amount0In", "type": "uint256"},
+                    {"indexed": False, "internalType": "uint256", "name": "amount1In", "type": "uint256"},
+                    {"indexed": False, "internalType": "uint256", "name": "amount0Out", "type": "uint256"},
+                    {"indexed": False, "internalType": "uint256", "name": "amount1Out", "type": "uint256"},
+                    {"indexed": True, "internalType": "address", "name": "to", "type": "address"}
+                ],
+                "name": "Swap",
+                "type": "event"
+            },
+            {
+                "anonymous": False,
+                "inputs": [
+                    {"indexed": False, "internalType": "uint112", "name": "reserve0", "type": "uint112"},
+                    {"indexed": False, "internalType": "uint112", "name": "reserve1", "type": "uint112"}
+                ],
+                "name": "Sync",
+                "type": "event"
+            }]
+        else:
+            # Handle other contract addresses as needed or raise an exception if the address is unknown
+            raise ValueError(f"No ABI found for contract address {contract_address}")
+
+
+    def start_monitor(self, start_block_number: int, watching_contracts: dict[str, list[str]]):
+        print(f"Starting monitor from block {start_block_number}...")
+        self.events_queue = []
+        for contract_address, events in watching_contracts.items():
+            contract_abi = self.fetch_contract_abi(contract_address)
+            contract = self.w3.eth.contract(address=contract_address, abi=contract_abi)
+            for event_name in events:
+                event_filter = getattr(contract.events, event_name).create_filter(fromBlock=start_block_number)
+                print(f"Monitoring for {event_name} events at address {contract_address}")
+                self.events_filters.append(event_filter)
+
+    def refresh(self):
+        print("Refreshing events...")
+        for filter in self.events_filters:
+            new_entries = filter.get_new_entries()
+            if new_entries:
+                print(f"Found {len(new_entries)} new events.")
+            self.events_queue.extend(new_entries)
 
     def poll(self):
-        """
-        :return: returns the next log in the iteration
-        """
-        pass
+        print("Polling for events...")
+        while not self.events_queue:
+            self.refresh()
+            time.sleep(1)  # Wait for a short time before trying to refresh again
+        event = self.events_queue.pop(0)
+        print(f"Polled event: {event}")
+        return event
 
     def stop(self):
-        pass
-
+        print("Stopping NodeStreaming...")
+        self.events_filters.clear()
 
 if __name__ == "__main__":
+    print("Starting the NodeStreaming service...")
+
     node_stream = NodeStreaming()
+    latest_block_number = node_stream.get_latest_block_number()
+    watching_contracts = {
+        "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0": ["Swap", "Sync"],
+    }
+    node_stream.start_monitor(start_block_number=latest_block_number, watching_contracts=watching_contracts)
+    event = node_stream.poll()
+    print(f"Event: {event}")
+    node_stream.stop()
+    print("NodeStreaming service has stopped.")
