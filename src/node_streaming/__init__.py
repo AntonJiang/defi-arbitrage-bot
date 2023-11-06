e06eb816e1c27cb0b938406973cf736f9732d001
William Almy <william.almy@skydio.com>
2023-11-05 17:33:54 -0800

    Refactor node_streaming for new dataclass

--- src/node_streaming/__init__.py
+++ src/node_streaming/__init__.py
@@ -19,8 +19,20 @@ class ContractEvent:
     raw_event: dict
     definition: ContractEventDefinition
 
+
+@dataclass
+class ContractEventFilter:
+    definition: ContractEventDefinition
+    event_filter: object
+
+
 class NodeStreaming:
-    def __init__(self, node_url: str, watching_contracts: list[ContractEventDefinition], start_block: Optional[int] = None):
+    def __init__(
+        self,
+        node_url: str,
+        watching_contracts: list[ContractEventDefinition],
+        start_block: Optional[int] = None,
+    ):
         print("Initializing NodeStreaming...")
         self.w3 = Web3(Web3.HTTPProvider(node_url))
 
@@ -29,32 +41,47 @@ class NodeStreaming:
         else:
             print("Connected to Ethereum node.")
 
-        self.events_queue = []
-        self.events_filters = []
+        self.events_queue: list[ContractEvent] = []
+        self.events_filters: list[ContractEventFilter] = []
 
         if start_block is None:
             start_block = self.get_latest_block_number()
+
         self.init_event_filters(start_block, watching_contracts)
 
-    def get_latest_block_number(self):
+    def get_latest_block_number(self) -> int:
         return self.w3.eth.block_number
 
-    def init_event_filters(self, start_block_number: int, watching_contracts: list[ContractEventDefinition]):
-        for contract_event in watching_contracts:
-            event_name = contract_event.event
-            contract = self.w3.eth.contract(address=contract_event.address, abi=get_event_abi(event_name))
-
-            event_filter = getattr(contract.events, event_name).create_filter(fromBlock=start_block_number)
-            print(f"Monitoring for {event_name} events at address {contract_event.address}")
-            self.events_filters.append(event_filter)
+    def init_event_filters(
+        self, start_block_number: int, watching_contracts: list[ContractEventDefinition]
+    ):
+        for contract_def in watching_contracts:
+            event_name = contract_def.event
+            contract = self.w3.eth.contract(
+                address=contract_def.address, abi=get_event_abi(event_name)
+            )
+            event_filter = getattr(contract.events, event_name).create_filter(
+                fromBlock=start_block_number
+            )
+            print(
+                f"Monitoring for {event_name} events at address {contract_event.address}"
+            )
+            self.events_filters.append(
+                ContractEventFilter(event_filter=event_filter, definition=contract_def)
+            )
 
     def refresh(self):
         print("Refreshing events...")
-        for filter in self.events_filters:
-            new_entries = filter.get_new_entries()
+        for contract_event_filter in self.events_filters:
+            contract_def = contract_event_filter.contract_def
+            event_filter = contact_event_filter.event_filter
+            new_entries = event_filter.get_new_entries()
             if new_entries:
                 print(f"Found {len(new_entries)} new events.")
-            self.events_queue.extend(new_entries)
+            for new_entry in new_entries:
+                self.events_queue.append(
+                    ContractEvent(definition=contract_def, raw_event=new_entry)
+                )
 
     def poll(self) -> ContractEvent:
         print("Polling for events...")
@@ -62,10 +89,7 @@ class NodeStreaming:
             self.refresh()
             time.sleep(5)  # Wait for a short time before trying to refresh again
         event = self.events_queue.pop(0)
-        print(f"Polled event: {event}")
-        """
-        TODO() return ContractEvent object here
-        """
+        print(f"Polled event: {event.raw_event}")
         return event
 
     def stop(self):
@@ -76,11 +100,17 @@ class NodeStreaming:
 if __name__ == "__main__":
     print("Starting the NodeStreaming service...")
 
-    node_stream = NodeStreaming(os.getenv("NODE_URL"),
-                                watching_contracts=[ContractEventDefinition("0x397FF1542f962076d0BFE58eA045FfA2d347ACa0",
-                                                                  "Swap"),
-                                                    ContractEventDefinition("0x397FF1542f962076d0BFE58eA045FfA2d347ACa0",
-                                                                  "Sync")])
+    node_stream = NodeStreaming(
+        os.getenv("NODE_URL"),
+        watching_contracts=[
+            ContractEventDefinition(
+                "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0", "Swap"
+            ),
+            ContractEventDefinition(
+                "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0", "Sync"
+            ),
+        ],
+    )
     event = node_stream.poll()
     print(f"Event: {event}")
     node_stream.stop()
--- src/node_streaming/abi.py
+++ src/node_streaming/abi.py
@@ -1,3 +1,11 @@
-
-
-ERC20_balance_of = [{"constant":True,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":False,"stateMutability":"view","type":"function"}]
+ERC20_balance_of = [
+    {
+        "constant": True,
+        "inputs": [{"name": "", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "", "type": "uint256"}],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function",
+    }
+]
